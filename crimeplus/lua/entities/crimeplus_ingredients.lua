a65638bf69e7d56bd976d505d79a8528317796b2
Billard <sirfrancisbillard@gmail.com>
2016-10-18 15:27:14 -1000

    Create crimeplus_ingredients.lua

---
+++ crimeplus/lua/entities/crimeplus_ingredients.lua
@@ -0,0 +1,13 @@
+AddCSLuaFile()
+
+CrimePlus.AddIngredient{
+	name = "Soil",
+	price = 50,
+	model = CrimePlus.Models["Soil"]
+}
+
+CrimePlus.AddIngredient{
+	name = "Fertilizer",
+	price = 50,
+	model = CrimePlus.Models["Fertilizer"]
+}
