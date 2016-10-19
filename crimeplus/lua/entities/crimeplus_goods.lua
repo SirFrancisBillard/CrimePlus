4442067fe9ba541907ea4dcd4fe1d280482c4c0a
Billard <sirfrancisbillard@gmail.com>
2016-10-18 15:17:25 -1000

    Registered sellable goods

--- crimeplus/lua/entities/crimeplus_goods.lua
+++ crimeplus/lua/entities/crimeplus_goods.lua
@@ -13,6 +13,7 @@ weed.price = CrimePlus.Config["Weed sell price"]
 weed.model = CrimePlus.Models["Weed bag"]
 weed.dealer = "crimeplus_dealer_drug"
 weed.color = Color(50, 160, 20)
+CrimePlus.AddMarketItem(weed)
 
 cocaine.name = "Cocaine"
 cocaine.id = "coke"
@@ -20,6 +21,7 @@ cocaine.price = CrimePlus.Config["Cocaine sell price"]
 cocaine.model = CrimePlus.Models["Cocaine brick"]
 cocaine.dealer = "crimeplus_dealer_drug"
 cocaine.color = Color(255, 230, 255)
+CrimePlus.AddMarketItem(cocaine)
 
 meth.name = "Methamphetamine"
 meth.id = "meth"
@@ -27,6 +29,7 @@ meth.price = CrimePlus.Config["Meth sell price"]
 meth.model = CrimePlus.Models["Meth bag"]
 meth.dealer = "crimeplus_dealer_drug"
 meth.color = Color(60, 240, 245)
+CrimePlus.AddMarketItem(meth)
 
 heroin.name = "Heroin"
 heroin.id = "heroin"
@@ -34,17 +37,20 @@ heroin.price = CrimePlus.Config["Heroin sell price"]
 heroin.model = CrimePlus.Models["Heroin needle"]
 heroin.dealer = "crimeplus_dealer_drug"
 heroin.color = Color(60, 240, 245)
+CrimePlus.AddMarketItem(heroin)
 
-heroin.name = "Crack Cocaine"
-heroin.id = "crack"
-heroin.price = CrimePlus.Config["Crack sell price"]
-heroin.model = CrimePlus.Models["Cocaine bag"]
-heroin.dealer = "crimeplus_dealer_drug"
-heroin.color = Color(60, 240, 245)
+crack.name = "Crack Cocaine"
+crack.id = "crack"
+crack.price = CrimePlus.Config["Crack sell price"]
+crack.model = CrimePlus.Models["Cocaine bag"]
+crack.dealer = "crimeplus_dealer_drug"
+crack.color = Color(60, 240, 245)
+CrimePlus.AddMarketItem(crack)
 
-heroin.name = "Illegal Weapons"
-heroin.id = "weapons"
-heroin.price = CrimePlus.Config["Weapons sell price"]
-heroin.model = CrimePlus.Models["Weapons"]
-heroin.dealer = "crimeplus_dealer_arms"
-heroin.color = Color(142, 68, 25)
+weps.name = "Illegal Weapons"
+weps.id = "weapons"
+weps.price = CrimePlus.Config["Weapons sell price"]
+weps.model = CrimePlus.Models["Weapons"]
+weps.dealer = "crimeplus_dealer_arms"
+weps.color = Color(142, 68, 25)
+CrimePlus.AddMarketItem(weps)
