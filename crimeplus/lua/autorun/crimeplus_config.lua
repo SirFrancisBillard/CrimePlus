9538b0bbd33f98a92f31d25ab8a3ceb7ceccc196
Billard <sirfrancisbillard@gmail.com>
2016-10-18 15:22:40 -1000

    Update crimeplus_config.lua

--- crimeplus/lua/autorun/crimeplus_config.lua
+++ crimeplus/lua/autorun/crimeplus_config.lua
@@ -7,6 +7,7 @@ CrimePlus.Config["Weed grow time"] = 90
 CrimePlus.Config["Weed grow time with fertilizer"] = 60
 CrimePlus.Config["Weed dry time"] = 45
 CrimePlus.Config["Weed grind time"] = 20
+CrimePlus.Config["Weed bags given"] = 4
 CrimePlus.Config["Weed sell price"] = 350
 
 -- Cocaine
@@ -15,6 +16,10 @@ CrimePlus.Config["Cocaine grind time"] = 20
 CrimePlus.Config["Cocaine mix time"] = 30
 CrimePlus.Config["Cocaine cook time"] = 90
 CrimePlus.Config["Cocaine sell price"] = 3200
+
+-- Heroin
+CrimePlus.Config["Heroin cook time"] = 90
+CrimePlus.Config["Heroin sell price"] = 850
 
 -- Crack
 CrimePlus.Config["Crack cook time"] = 90
