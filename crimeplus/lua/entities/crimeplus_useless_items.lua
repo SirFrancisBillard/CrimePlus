9c4ded1f984f095cbb9acbae13f5eaa48b08ff85
Billard <sirfrancisbillard@gmail.com>
2016-10-19 14:33:59 -1000

    Create crimeplus_useless_items.lua

---
+++ crimeplus/lua/entities/crimeplus_useless_items.lua
@@ -0,0 +1,19 @@
+AddCSLuaFile()
+
+CrimePlus.RegisterUselessItem{
+	name = "Water",
+	class = "crimeplus_water",
+	model = CrimePlus.Models["Water bottle"]
+}
+
+CrimePlus.RegisterUselessItem{
+	name = "Soil",
+	class = "crimeplus_soil",
+	model = CrimePlus.Models["Soil"]
+}
+
+CrimePlus.RegisterUselessItem{
+	name = "Fertilizer",
+	class = "crimeplus_fertilizer",
+	model = CrimePlus.Models["Fertilizer"]
+}
