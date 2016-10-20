a3ffc5ece64b1e6e691335a3baab78660cc4d101
Billard <sirfrancisbillard@gmail.com>
2016-10-19 14:53:47 -1000

    Update and rename crimeplus_useless_items.lua to crimeplus_items.lua

---
+++ crimeplus/lua/entities/crimeplus_items.lua
@@ -0,0 +1,74 @@
+AddCSLuaFile()
+
+CrimePlus.RegisterUselessItem{
+	name = "Water",
+	class = "crimeplus_water",
+	model = CrimePlus.Models["Water bottle"]
+}
+
+CrimePlus.RegisterUselessItem{
+	name = "Soil",
+	class = "crimeplus_soil",
+	model = CrimePlus.Models["Soil"]
+}
+
+CrimePlus.RegisterUselessItem{
+	name = "Fertilizer",
+	class = "crimeplus_fertilizer",
+	model = CrimePlus.Models["Fertilizer"]
+}
+
+CrimePlus.RegisterUselessItem{
+	name = "Ephedrine",
+	class = "crimeplus_ephedrine",
+	model = CrimePlus.Models["Ephedrine"]
+}
+
+CrimePlus.RegisterUselessItem{
+	name = "Red Phosphorus",
+	class = "crimeplus_red_phosphorus",
+	model = CrimePlus.Models["Red phosphorus"]
+}
+
+CrimePlus.RegisterUselessItem{
+	name = "Iodine",
+	class = "crimeplus_iodine",
+	model = CrimePlus.Models["Iodine"]
+}
+
+CrimePlus.RegisterUselessItem{
+	name = "Hydrochloric Acid",
+	class = "crimeplus_hydro_acid",
+	model = CrimePlus.Models["Hydrochloric acid"]
+}
+
+CrimePlus.RegisterUselessItem{
+	name = "Ether",
+	class = "crimeplus_ether",
+	model = CrimePlus.Models["Ephedrine"]
+}
+
+CrimePlus.RegisterUselessItem{
+	name = "Ammonia",
+	class = "crimeplus_ammonia",
+	model = CrimePlus.Models["Ammonia"]
+}
+
+CrimePlus.RegisterUselessItem{
+	name = "Sodium Hydroxide",
+	class = "crimeplus_sodium_hydro",
+	model = CrimePlus.Models["Sodium hydroxide"]
+}
+
+CrimePlus.RegisterPotIngredient("crimeplus_ephedrine", "ephedrine")
+CrimePlus.RegisterPotIngredient("crimeplus_red_phosphorus", "red phosphorus")
+CrimePlus.RegisterPotIngredient("crimeplus_iodine", "iodine")
+CrimePlus.RegisterPotIngredient("crimeplus_hydro_acid", "hydrochloric acid")
+CrimePlus.RegisterPotIngredient("crimeplus_ether", "ether")
+CrimePlus.RegisterPotIngredient("crimeplus_ammonia", "ammonia")
+CrimePlus.RegisterPotIngredient("crimeplus_sodium_hydro", "sodium hydroxide")
+CrimePlus.RegisterPotIngredient("crimeplus_coca_paste", "coca paste")
+CrimePlus.RegisterPotIngredient("crimeplus_caustic", "caustic soda")
+CrimePlus.RegisterPotIngredient("crimeplus_cocaine_solution", "cocaine")
+CrimePlus.RegisterPotIngredient("crimeplus_baking_soda", "baking soda")
+CrimePlus.RegisterPotIngredient("crimeplus_vinegar", "vinegar")
